9d366cf6b95f3ca7bb2a11110f3460b24507de70
Robert Welch <robet.welch33@gmail.com>
2022-05-23 19:27:41 +0200

    Added GUI entry for max quiescent nuclei area

--- src/gui.py
+++ src/gui.py
@@ -1,5 +1,7 @@
+from sqlite3 import Row
 import tkinter as tk
 from tkinter.filedialog import askdirectory
+from tkinter.tix import COLUMN, ROW
 
 
 class GUI(tk.Tk):
@@ -8,8 +10,9 @@ class GUI(tk.Tk):
 
         # Create Frame
         self.frame = tk.Frame()
-        self.frame.pack()
+        self.frame.grid(columnspan=1,rowspan=4,sticky=tk.W+tk.E)
         self.title("Scenescent Analysis")
+        self.frame.grid_rowconfigure((0,2),weight=1)
 
         # Get Directory Input Button
         directory_text = tk.StringVar()
@@ -21,26 +24,39 @@ class GUI(tk.Tk):
             height=2,
             width=50,
         )
-        browse_btun.grid(column=1, row=0)
+        browse_btun.grid(column=0, row=0,padx=10, pady=10)
 
-        # Run Analysis Button - Just closes GUI
+        #Take maximum quiescent size
+        var = tk.StringVar()
+        max_quiescent_area_label = tk.Label(self.frame,text="Maximum area (pixels\u00b2) of quiescent nuclei ")
+        max_quiescent_area_label.grid(column=0, row=1,padx=5, pady=0)
+        self.max_quiescent_area_entry = tk.Entry(self.frame, textvariable=var)
+        self.max_quiescent_area_entry.insert(tk.END, "1000")
+        self.max_quiescent_area_entry.grid(column=0, row=2,padx=5, pady=0)
+
+        # Run Analysis Button
         run_text = tk.StringVar()
         run_text.set("Run Analysis")
         run_btun = tk.Button(
             self.frame,
             textvariable=run_text,
-            command=lambda: self.quit_gui(),
+            command=lambda: self.getInputs(),
             height=2,
             width=10,
         )
-        run_btun.grid(column=1, row=1)
+        run_btun.grid(column=0, row=3,padx=10, pady=10)
 
     # Choose Image Directory
     def select_directory(self):
         browse_text = tk.StringVar()
         browse_text.set("loading...")
         self.directory = askdirectory(parent=self.frame)
-        browse_text.set(self.directory)
+        #browse_text.set(self.directory)
 
-    def quit_gui(self):
+    # Save inputs and close GUI
+    def getInputs(self):
+        self.max_quiescent_area = self.max_quiescent_area_entry.get()
         self.after(100, self.destroy())
+
+
+   
--- src/main.py
+++ src/main.py
@@ -1,15 +1,13 @@
 from functions import main_analysis
 from gui import GUI
 
-
 def main():
     print("Select Directory in GUI and press Run Analysis")
     gui = GUI()
     gui.mainloop()
     print("Directory chosen.")
-    main_analysis(gui.directory)
+    main_analysis(gui.directory,gui.max_quiescent_area)
     return None
-
 
 if __name__ == "__main__":
     main()
