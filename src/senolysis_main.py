1d29b39ed022e68b8d9f4933dc36a9bb4e73a1e1
robertw33 <robert.welch33@gmail.com>
2022-06-09 10:15:57 +0200

    updated to take user_parameters class

--- src/senolysis_main.py
+++ src/senolysis_main.py
@@ -5,8 +5,9 @@ def main():
     print("Select Directory in GUI and press Run Analysis")
     gui = GUI()
     gui.mainloop()
+    user_parameters = gui.get_user_parameters()
     print("Directory chosen.")
-    main_analysis(gui.directory,gui.max_quiescent_area)
+    main_analysis(user_parameters)
     return None
 
 if __name__ == "__main__":
