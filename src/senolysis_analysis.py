3cbfa546ae6c966c49d0e8f974cf75452beca3db
robertw33 <robert.welch33@gmail.com>
2022-06-16 14:00:15 +0200

    adjusted determine_nuclei_type

--- src/senolysis_analysis.py
+++ src/senolysis_analysis.py
@@ -70,7 +70,8 @@ def senolysis_analysis(img_path, program_start_time):
 
     write_csv(results_dataframe, os.path.join(save_path, "Senolysis_measures.csv"))
 
-    RGB = np.dstack([red_norm, green_norm, blue_norm])
+    #RGB = np.dstack([normalize_img(red,99), normalize_img(green,99), normalize_img(blue,99)])
+    RGB = np.dstack([red_norm,green_norm,blue_norm])
 
     img_name = os.path.split(img_path)[-1]
     img_name = os.path.splitext(img_name)[0]
